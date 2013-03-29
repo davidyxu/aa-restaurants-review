63d8c26e6218fdfab3ffcd4da11f1d2d233a8d54
David Xu <dvdyxu@gmail.com>
2013-03-29 10:35:29 -0700

    add seed data for db

--- db_schema.sql
+++ db_schema.sql
@@ -5,19 +5,20 @@ CREATE TABLE chef (
 	mentor INTEGER,
 	FOREIGN KEY (mentor) REFERENCES chef(id)
 );
+
 CREATE TABLE restaurant (
 	id INTEGER PRIMARY KEY,
 	neighborhood VARCHAR(255) NOT NULL,
-	cuisine VARCHAR 255 NOT NULL
+	cuisine VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cheftenure (
 	id INTEGER PRIMARY KEY,
 	chef_id INTEGER NOT NULL,
-	start_date INTEGER NOT NULL,
-	end_date INTEGER,
+	start_date DATE NOT NULL,
+	end_date DATE,
 	head_chief INTEGER NOT NULL,
-	FOREIGN KEY (chief_id) REFERENCES chief(id)
+	FOREIGN KEY (chef_id) REFERENCES chef(id)
 );
 
 CREATE TABLE critic (
@@ -27,15 +28,31 @@ CREATE TABLE critic (
 
 CREATE TABLE restaurantreview (
 	id INTEGER PRIMARY KEY,
+	restaurant_id INTEGER NOT NULL,
+	critic_id INTEGER NOT NULL,
 	text_review TEXT NOT NULL,
 	score INTEGER NOT NULL,
-	review_date INTEGER NOT NULL
+	review_date DATE NOT NULL,
+	FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
+	FOREIGN KEY (critic_id) REFERENCES critic(id)
 );
 
 INSERT INTO chef (first_name, last_name, mentor)
-VALUES ('Bob', 'Smith', NULL)
+VALUES ('Bob', 'Smith', NULL), ('Delish', 'Cook', 1);
 
 INSERT INTO restaurant (neighborhood, cuisine)
-VALUES ('')
+VALUES ('yumville', 'yummy cuisine'), ('yuckville', 'yucky cuisine');
 
-INSERT INTO cheftenure (start_date, end_date, head_chief)
+INSERT INTO cheftenure (chef_id, start_date, end_date, head_chief)
+VALUES (1, '1999-12-20', NULL, 1),
+			 (1, '2001-02-25', '2001-08-13', 0),
+			 (2, '2002-01-02', NULL, 1);
+
+INSERT INTO critic (screen_name)
+VALUES ('hateful critic'), ('nice critic'), ('boring critic');
+
+INSERT INTO restaurantreview (restaurant_id, critic_id, text_review, score, review_date)
+VALUES (1, 1, 'horrible food', 1, '2012-05-05'),
+			 (2, 1, 'terrible food', 1, '2013-03-02'),
+			 (2, 2, 'amazing', 20, '2013-02-04'),
+			 (1, 3, 'ok.', 10, '2013-03-15');
--- restaurants.rb
+++ restaurants.rb
@@ -1,15 +1,28 @@
-class Chef
 
+
+class Chef
+	attr_accessor :first_name :last_name
+	attr_reader :id
+	def mentor
+	end
+	def name
+	end
+	def tenures
+	end
 end
 
 class Restaurant
+	
 end
 
 class ChefTenure
+
 end
 
 class Critic
+
 end
 
 class Restaurant Review
+
 end
